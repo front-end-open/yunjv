a78d1265dc1422342888c02607e71872efdb37fc
CNine <cnine229@gmail.com>
2020-05-06 22:40:46 +0800

    refactor: 分离样式到 main.less

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,3 @@
     <router-view />
   </div>
 </template>
-
-<style lang="less">
-body {
-  color: #505050;
-  font: 12px -apple-system, BlinkMacSystemFont, Helvetica Neue, Helvetica, Arial,
-    PingFang SC, Hiragino Sans GB, Microsoft YaHei, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-.fa-icon {
-  width: auto;
-  height: 1.2em;
-  max-width: 100%;
-  max-height: 100%;
-}
-</style>
