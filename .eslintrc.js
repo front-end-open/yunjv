3af0efab33d0b4f950320f16bb7efe71e984f213
CNine <cnine229@gmail.com>
2020-05-22 23:16:10 +0800

    chore(lint): 关闭 no-console

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,8 @@ module.exports = {
     parser: 'babel-eslint',
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'prettier/prettier': [
       'warn',
