d2a3df2bd725e8dba9a938f9f8f2948ccbacd95b
PachVerb <1506262681@qq.com>
2020-07-16 00:24:54 +0800

    style: 代码格式调整 lib

--- src/lib/renameDir_File.js
+++ src/lib/renameDir_File.js
@@ -1,6 +1,5 @@
 function rename(This) {
-<<<<<<< HEAD
-  console.log(This)
+  console.log(This, 'lib 目录 renameDir Tools')
   if (This.path == '/') {
     This.$set(This.tableData, This.formDate.id, {
       'id': Math.random(),
@@ -26,33 +25,5 @@ function rename(This) {
       'path': `${This.path}/${This.formDate.name}`,
     })
   }
-=======
-    console.log(This)
-    if (This.path == '/') {
-        This.$set(This.tableData, This.formDate.id, {
-            'id': Math.random(),
-            'isdir': This.tableData[This.formDate.id].isdir,
-            'parent': This.tableData[This.formDate.id].parent,
-            'local_mtime': This.tableData[This.formDate.id].local_mtime,
-            'parentsPath': This.tableData[This.formDate.id].parentsPath,
-            'repos_id': This.tableData[This.formDate.id].repos_id,
-            'server_filename': This.formDate.name,
-            'size': This.tableData[This.formDate.id].size,
-            'path': `${This.path}${This.formDate.name}`,
-        })
-    } else {
-        This.$set(This.tableData, This.formDate.id, {
-            'id': Math.random(),
-            'isdir': This.tableData[This.formDate.id].isdir,
-            'local_mtime': This.tableData[This.formDate.id].local_mtime,
-            'parent': This.tableData[This.formDate.id].parent,
-            'parentsPath': This.tableData[This.formDate.id].parentsPath,
-            'repos_id': This.tableData[This.formDate.id].repos_id,
-            'server_filename': This.formDate.name,
-            'size': This.tableData[This.formDate.id].size,
-            'path': `${This.path}/${This.formDate.name}`,
-        })
-    }
->>>>>>> feat(filelist.vue): 右击菜单
 }
 export default rename
