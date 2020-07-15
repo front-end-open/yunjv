84c5bfe5303b5c36e76b6e252685adb70a9823b1
you name <you@example.com>
2020-07-15 20:59:30 +0800

    style(filelist.vue): 修改样式

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,16 +11,17 @@ module.exports = {
     // 'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'prettier/prettier': [
-      'warn',
-      {
-        singleQuote: true,
-        semi: false,
-        trailingComma: 'all',
-        quoteProps: 'preserve',
-        arrowParens: 'always',
-      },
-    ],
+    "prettier/prettier": "off",
+    // 'prettier/prettier': [
+    //   'warn',
+    //   {
+    //     singleQuote: true,
+    //     semi: false,
+    //     trailingComma: 'all',
+    //     quoteProps: 'preserve',
+    //     arrowParens: 'always',
+    //   },
+    // ],
   },
   overrides: [
     {
