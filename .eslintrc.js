a6362667139cf086b8873800a3bc9e051f298182
PachVerb <1506262681@qq.com>
2020-07-16 00:18:20 +0800

    docs: eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    "prettier/prettier": "off",
     'prettier/prettier': [
       'warn',
       {
